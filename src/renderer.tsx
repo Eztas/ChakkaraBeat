f65eb083da875dae959411286452fc26a2f224a8
Eztas <nietzsche2669sensei@gmail.com>
2026-04-29 19:07:36 +0900

    del: SSRなので削除

--- src/renderer.tsx
+++
@@ -1,9 +0,0 @@
-import { jsxRenderer } from 'hono/jsx-renderer'
-
-export const renderer = jsxRenderer(({ children }) => {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  )
-})
